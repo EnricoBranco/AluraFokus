35a456baa67b49e6b5f4ff319dcaee0171706727
EnricoBranco <enrico.branco@univille.com.br>
2023-12-31 15:03:46 -0300

    curso finalizado

--- script.js
+++ script.js
@@ -3,13 +3,15 @@ const modosBt = document.querySelectorAll('.app__card-button')
 const banner = document.querySelector('.app__image')
 const titulo = document.querySelector('.app__title')
 const startBt = document.querySelector('#alternar-musica')
+const comecarBt = document.querySelector('#start-pause')
+const tempoNaTela = document.querySelector('#timer')
 const musica = new Audio ('sons/luna-rise-part-one.mp3')
 const playAudio = new Audio('sons/play.wav')
 const pauseAudio = new Audio('sons/pause.mp3')
 const fimAudio = new Audio('sons/beep.mp3')
-const comecarBt = document.querySelector('#start-pause')
+
 let intervaloId
-let segundosDec = 5
+let segundosDec = 1500
 
 musica.loop = true
 
@@ -18,6 +20,7 @@ function alterarBotao(i) {
 }
 
 function alterarContexto(contexto) {
+    mostrarTempo()
     html.setAttribute('data-contexto', contexto)
     banner.setAttribute('src', `/imagens/${contexto}.png`)
     modosBt.forEach(function(elemento) {
@@ -41,16 +44,19 @@ function alterarContexto(contexto) {
 }
 
 modosBt[0].addEventListener('click', () => {
+    segundosDec = 1500;
     alterarContexto('foco')
     alterarBotao(0)
 })
 
 modosBt[1].addEventListener('click', () => {
+    segundosDec = 300;
     alterarContexto('descanso-curto')
     alterarBotao(1)
 })
 
 modosBt[2].addEventListener('click', () => {
+    segundosDec = 900;
     alterarContexto('descanso-longo')
     alterarBotao(2)
 })
@@ -66,31 +72,41 @@ startBt.addEventListener('change', () => {
 
 const contagemRegressiva = () => {
     if (segundosDec <= 0) {
-        //fimAudio.play()
+        fimAudio.play()
         alert('acabou o tempo')
         zerar()
         return
     }
     segundosDec -= 1
-    console.log('Temporizador: ' + segundosDec) 
+    mostrarTempo()
 }
 
 comecarBt.addEventListener('click', () => {
-    iniciarPausar()
-})
-
-function iniciarPausar() {
     if (intervaloId) {
-        pauseAudio.play()
+        console.log(intervaloId)
         zerar()
         return
     }else {
+        console.log(intervaloId)
         playAudio.play()
         intervaloId = setInterval(contagemRegressiva, 1000)
+        comecarBt.childNodes[3].textContent = 'Pause'
+        comecarBt.childNodes[1].setAttribute('src', 'imagens/pause.png')
     }
-}
+})
 
 function zerar() {
     clearInterval(intervaloId)
+    comecarBt.childNodes[3].textContent = 'Começar'
+    comecarBt.childNodes[1].setAttribute('src', 'imagens/play_arrow.png')
     intervaloId = null
+    pauseAudio.play()
 }
+
+function mostrarTempo() {
+    const tempo = new Date(segundosDec * 1000)
+    const tempoFormatado = tempo.toLocaleTimeString('pt-br', {minute: '2-digit', second: '2-digit'})
+    tempoNaTela.innerHTML = `${tempoFormatado}`
+}
+
+mostrarTempo()
